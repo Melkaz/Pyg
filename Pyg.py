e7e3963200e4628a9a0ac289002104cbf01eaccd
Yohann <yohann.nedelec@gmail.com>
2015-02-08 22:48:31 +0100

    Initial commit

---
+++ Pyg.py
@@ -0,0 +1,43 @@
+import re
+import gzip
+
+def read_gtf(gtf_file, interesting_features = ['gene', 'transcript', 'exon']):
+    elements = list()
+
+    if gtf_file.lower().endswith('.gz'):
+        opener = gzip.open
+    else:
+        opener = open
+
+    with opener(gtf_file, 'rt') as f:
+        for line in f:
+            fields = line.strip().split('\t')
+
+            try:
+                feature = fields[2]
+            except IndexError:
+                # Looks like we picked a comment line!
+                continue
+
+            if feature not in interesting_features:
+                continue
+
+            names = ['seqname', 'source', 'feature', 'start', 'end', 'score', 'strand', 'frame', 'attribute']
+            first_dict = dict(zip(names, fields))
+
+            info_field = fields[8].replace('"','').strip(';')
+            info_fields = re.split('; | ', info_field)
+            second_dict = dict(zip(*[iter(info_fields)]*2))
+
+            element = {}
+            element.update(first_dict) or element.update(second_dict)
+
+            elements.append(element)
+
+    return elements
+
+if __name__ == '__main__':
+    gtf_file = "Data/Homo_sapiens.GRCh37.75.SAMPLE.gtf.gz"
+    # gtf_file = "Data/Homo_sapiens.GRCh37.75.SAMPLE.gtf"
+    features = read_gtf(gtf_file)
+    print(features[1:10])
---
+++ README.md
@@ -0,0 +1,5 @@
+# Pyg
+Pyg is a gtf-file reader for python
+
+Basically, it helps you parse those strange files to return a list of Python dictionnaries.
+Pyg can read gtf or gzip compressed files.
